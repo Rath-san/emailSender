a2cc4fbbe465a4dc2b2a3f0f9a18168d3612d3de
Rath-san <rim.kozlowski@gmail.com>
2020-06-14 20:22:06 +0200

    Added whitelisting cors

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-
-const port = process.env.PORT || 4000
-
 require('dotenv').config({ silent: process.env.NODE_ENV !== 'developement' });
 
+const port = process.env.PORT || 4000;
 
-const sendEmail = ({name, email, message}, onError, onSuccess) => {
-
+const sendEmail = ({ name, email, message }, onError, onSuccess) => {
     const transporter = nodemailer.createTransport({
         service: process.env.SERVICE,
         auth: {
@@ -27,12 +24,24 @@ const sendEmail = ({name, email, message}, onError, onSuccess) => {
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
-        if(error) {
-            onError(error)
+        if (error) {
+            onError(error);
         } else {
             onSuccess('Email sent');
         }
     });
+};
+
+const whitelist = process.env.CORS_WHITELIST.split(',');
+
+const corsOptionsDelegate = function (req, callback) {
+    let corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
 const app = express();
@@ -40,19 +49,16 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.options('/send', cors())
+app.options('/send', cors(corsOptionsDelegate));
 app.post('/send', cors(), (req, res) => {
     sendEmail(
         req.body,
         (err) => {
-            res
-                .sendStatus(500)
-                .send('Internal server error')
+            res.sendStatus(500).send('Internal server error');
         },
-        () => {
-            res
-                .sendStatus(200)
-                .send('OK')
+        (message) => {
+            console.info(message);
+            res.sendStatus(200).send('OK');
         }
     );
 });

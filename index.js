bbb3926060fd034e1098872a7215d08754e1f0a4
Rath-san <rim.kozlowski@gmail.com>
2020-06-14 15:50:02 +0200

    Init repo

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const sendEmail = ({name, email, message}, onError, onSuccess) => {
+
+    const transporter = nodemailer.createTransport({
+        service: process.env.SERVICE,
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASS,
+        },
+    });
+
+    const mailOptions = {
+        from: email,
+        sender: email,
+        to: process.env.TO,
+        subject: `${name} | new message !`,
+        text: message,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            onError(error)
+        } else {
+            onSuccess('Email sent');
+        }
+    });
+};
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.options('/send', cors())
+app.post('/send', cors(), (req, res) => {
+    sendEmail(
+        req.body,
+        (err) => {
+            res
+                .sendStatus(500)
+                .send('Internal server error')
+        },
+        () => {
+            res
+                .sendStatus(200)
+                .send('OK')
+        }
+    );
+});
+
+app.listen(4000, () => console.log('server started @ 4000'));
